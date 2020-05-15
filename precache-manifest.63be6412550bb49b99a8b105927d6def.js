b7fe72cd4ad83fb689c8fde34d700d1bbb119059
Batura Andrey <batura.andrey@otr.ru>
2020-05-15 17:56:39 +0300

    add: gh-pages for hosted from GitHub repository 👽

---
+++ precache-manifest.63be6412550bb49b99a8b105927d6def.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4ffe57fc8c40a39c23f9b669a11211",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2fab5358732e611038a1",
+    "url": "./static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "4a3261427463930c5184",
+    "url": "./static/css/main.c9ee5218.chunk.css"
+  },
+  {
+    "revision": "2fab5358732e611038a1",
+    "url": "./static/js/2.273fc392.chunk.js"
+  },
+  {
+    "revision": "5ad387cd57eb31f6f50768be47fe3b09",
+    "url": "./static/js/2.273fc392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3261427463930c5184",
+    "url": "./static/js/main.e790b3bb.chunk.js"
+  },
+  {
+    "revision": "2aa317e07df8ca567521",
+    "url": "./static/js/runtime-main.15f284ee.js"
+  }
+]);
